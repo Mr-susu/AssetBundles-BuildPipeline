3e63d94b1f61b0df3996288bd32b62472444b573
Ryan Caltabiano <ryanc@unity3d.com>
2017-12-15 15:28:55 -0600

    TODO comment

--- UnityEditor.Build/Editor/AssetBundleBuildPipeline/Shared/BuildPipeline.cs
+++ UnityEditor.Build/Editor/AssetBundleBuildPipeline/Shared/BuildPipeline.cs
@@ -57,7 +57,7 @@ namespace UnityEditor.Build
             // Dependency
             AddDependencyTasks(pipeline);
             // Packing
-            AddPackingTasks(pipeline, new Unity5PackedIdentifiers());
+            AddPackingTasks(pipeline, new Unity5PackedIdentifiers()); // TODO: Replace with PrefabPackedIdentifiers when stable
             // Writing
             AddWritingTasks(pipeline);
             return pipeline;
@@ -71,7 +71,7 @@ namespace UnityEditor.Build
             // Dependency
             AddDependencyTasks(pipeline);
             // Packing
-            AddPackingTasks(pipeline, new Unity5PackedIdentifiers());
+            AddPackingTasks(pipeline, new Unity5PackedIdentifiers()); // TODO: Replace with PrefabPackedIdentifiers when stable
             // Writing
             AddWritingTasks(pipeline);
             return pipeline;
