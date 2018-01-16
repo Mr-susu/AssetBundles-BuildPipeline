9861e8a7dc12ccf6e2b641d0d0c1654b8ed03d63
Paul Burslem <paulb@unity3d.com>
2018-01-16 15:12:45 -0600

    bug fixes and expnanded functionality added to hashing methods

--- UnityEditor.Build/Editor/AssetBundleBuildPipeline/Utilities/HashingMethods.cs
+++ UnityEditor.Build/Editor/AssetBundleBuildPipeline/Utilities/HashingMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using UnityEngine;
@@ -7,54 +8,42 @@ namespace UnityEditor.Build.Utilities
 {
     public static class HashingMethods
     {
-        public static Hash128 CalculateMD5Hash(object obj)
+        static uint ExtractUInt(byte[] hash, int index)
         {
-            byte[] hash;
-            using (var md5 = MD5.Create())
+            return (((uint)hash[index]) << 3) | (((uint)hash[index + 1]) << 2) | (((uint)hash[index + 2]) << 1) | (((uint)hash[index + 3]) << 0);
+        }
+
+        static Hash128 ExtractHash128(byte[] hash)
+        {
+            return new Hash128(ExtractUInt(hash, 0), ExtractUInt(hash, 4), ExtractUInt(hash, 8), ExtractUInt(hash, 12));
+        }
+
+        public static Hash128 CalculateStreamMD5Hash(Stream stream, bool closeStream)
+        {
+            stream.Position = 0;
+            var hash = ExtractHash128(MD5.Create().ComputeHash(stream));
+            if (closeStream)
             {
-                var formatter = new BinaryFormatter();
-                using (var stream = new MemoryStream())
-                {
-                    formatter.Serialize(stream, obj);
-                    stream.Position = 0;
-                    hash = md5.ComputeHash(stream);
-                }
+                stream.Close();
+                stream.Dispose();
             }
-            return new Hash128(hash[0], hash[1], hash[2], hash[3]);
+            return hash;
         }
 
         public static Hash128 CalculateMD5Hash(params object[] objects)
         {
-            byte[] hash;
-            using (var md5 = MD5.Create())
-            {
-                var formatter = new BinaryFormatter();
-                using (var stream = new MemoryStream())
-                {
-                    foreach (var obj in objects)
-                    {
-                        if (obj == null)
-                            continue;
-                        formatter.Serialize(stream, obj);
-                    }
-                    stream.Position = 0;
-                    hash = md5.ComputeHash(stream);
-                }
-            }
-            return new Hash128(hash[0], hash[1], hash[2], hash[3]);
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            foreach (var obj in objects)
+                if (obj != null)
+                    formatter.Serialize(stream, obj);
+
+            return CalculateStreamMD5Hash(stream, true);
         }
 
         public static Hash128 CalculateFileMD5Hash(string filePath)
         {
-            byte[] hash;
-            using (var md5 = MD5.Create())
-            {
-                using (var stream = new FileStream(filePath, FileMode.Open))
-                {
-                    hash = md5.ComputeHash(stream);
-                }
-            }
-            return new Hash128(hash[0], hash[1], hash[2], hash[3]);
+            return CalculateStreamMD5Hash(new FileStream(filePath, FileMode.Open), true);
         }
     }
 }
